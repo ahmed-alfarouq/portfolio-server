7177ee768b6cd982b46f669ed77e84ed588669c7
ahmed-al-farouq <mediocre23534@gmail.com>
2025-08-13 16:02:38 +0300

    Add health check endpoint to server

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", emailRoutes);
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
